7889ecc8fafe78ea1bd05692bec9ad8f610a24e6
Andres Vaquero <web.andresvaquero@gmail.com>
2025-03-09 00:43:15 +1100

    removing ts ignores

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,3 @@
 export { IconMenuConfigType } from './components/iconMenu/iconMenu.types';
 export { NavLinkConfigType } from './components/navLink/navLink.types';
-export { NavListConfigType } from './components/navList/navList.types';
+export { NavListConfigType, SelectedCallbackPayloadType } from './components/navList/navList.types';
