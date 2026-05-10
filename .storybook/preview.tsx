96a5e5f7863ceccc47e8c18ac28685d802e832c9
Andres Vaquero <web.andresvaquero@gmail.com>
2026-05-10 15:22:31 +1000

    usage addon refactor (support tsx)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,7 +3,6 @@ import { setService } from '@arpadroid/context';
 import { Router, APIService } from '@arpadroid/services';
 import { mergeObjects } from '@arpadroid/tools';
 import defaultConfig from '@arpadroid/module/storybook/preview';
-/** @type { import('@storybook/web-components-vite').Preview } */
 const config = mergeObjects(
     defaultConfig,
     {
